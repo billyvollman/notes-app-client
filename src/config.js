b40b7dbb96fc69c8583e15b3d8863962177669e9
billyvollman <b.vollman@gmail.com>
2019-10-30 16:59:21 +1100

    setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "billys-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://nk8ty9pkea.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_Jij2gi9U0",
+      APP_CLIENT_ID: "kap36n5pqlalucks7c6q3bnae",
+      IDENTITY_POOL_ID: "us-east-2:fac92fb0-c06b-4ea6-a14e-36ef5486ae91"
+    }
+};
